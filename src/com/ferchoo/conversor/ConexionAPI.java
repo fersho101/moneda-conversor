96de20588230f4407c12138c65e160883f7e9b84
fernando corral <146652466+fersho101@users.noreply.github.com>
2025-04-24 08:18:21 -0700

    creando config.properties

--- src/com/ferchoo/conversor/ConexionAPI.java
+++ src/com/ferchoo/conversor/ConexionAPI.java
@@ -1,13 +1,26 @@
 package com.ferchoo.conversor;
 
+import java.io.FileInputStream;
 import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class ConexionAPI {
-    private static final String API_KEY = "a5bb8a0eb0f7c839d51e9a0e";
+    private static String API_KEY;
+
+    static {
+        try{
+            Properties props = new Properties();
+            props.load(new FileInputStream("/src/config.properties"));
+            API_KEY = props.getProperty("API_KEY");
+        } catch (Exception e) {
+            System.err.println(" Error al cargar la API key: " + e.getMessage());
+        }
+    }
+
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
 
     public static String obtenerTazasDeCambio(String monedaBase) throws Exception {
